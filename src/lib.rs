9b34d552ec64813c4367f57b62d7f8dbb1c80be1
Min Shao <mshao@petabi.com>
2019-06-11 14:16:43 -0700

    init dbscan

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,17 @@
+mod dbscan;
 
+pub use dbscan::DBSCAN;
+
+pub trait Fit {
+    type Input;
+    type Output;
+
+    fn fit(&mut self, input: Self::Input) -> Self::Output;
+}
+
+pub trait Predict {
+    type Input;
+    type Output;
+
+    fn predict(&mut self, input: Self::Input) -> Self::Output;
+}
