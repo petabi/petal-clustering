d09af29afd6cb3e4a9b097cb13e92da965836227
Min Shao <mshao@petabi.com>
2019-10-28 17:32:40 -0700

    add skeleton for optics

--- src/optics.rs
+++ src/optics.rs
@@ -1,4 +1,4 @@
-use ndarray::ArrayView2;
+use ndarray::{ArrayView1, ArrayView2};
 use petal_neighbors::BallTree;
 use rayon::prelude::*;
 use std::collections::HashMap;
@@ -33,9 +33,104 @@ impl<'a> Fit<'a> for Optics {
         if input.is_empty() {
             return (HashMap::new(), vec![]);
         }
-        let _neighborhoods = build_neighborhoods(&input, self.eps);
+        let neighborhoods = build_neighborhoods(&input, self.eps);
+        let mut visited = vec![false; input.nrows()];
+        let mut ordered: Vec<usize> = Vec::with_capacity(input.nrows());
+        let mut reacheability = vec![std::f64::NAN; input.nrows()];
+        for (idx, n) in neighborhoods.iter().enumerate() {
+            if visited[idx] || n.neighbors.len() < self.min_samples {
+                continue;
+            }
+            process(
+                &input,
+                &mut ordered,
+                &mut reacheability,
+                &mut visited,
+                idx,
+                self.min_samples,
+                &neighborhoods,
+            );
+        }
         (HashMap::new(), vec![])
     }
+}
+
+fn process<'a>(
+    input: &ArrayView2<'a, f64>,
+    ordered: &mut Vec<usize>,
+    reacheability: &mut Vec<f64>,
+    visited: &mut [bool],
+    idx: usize,
+    min_samples: usize,
+    neighborhoods: &[Neighborhood],
+) {
+    let mut to_visit = vec![idx];
+    while let Some(cur) = to_visit.pop() {
+        if visited[cur] {
+            continue;
+        }
+        visited[cur] = true;
+        ordered.push(cur);
+        if neighborhoods[cur].neighbors.len() < min_samples {
+            continue;
+        }
+        let mut seeds = vec![];
+        update(
+            input,
+            &neighborhoods[cur],
+            cur,
+            &mut seeds,
+            &visited,
+            reacheability,
+        );
+        while let Some(s) = seeds.pop() {
+            if visited[s] {
+                continue;
+            }
+            visited[s] = true;
+            ordered.push(s);
+            let n = &neighborhoods[s];
+            if n.neighbors.len() < min_samples {
+                continue;
+            }
+            update(
+                input,
+                &neighborhoods[cur],
+                cur,
+                &mut seeds,
+                &visited,
+                reacheability,
+            );
+        }
+    }
+}
+
+fn update<'a>(
+    input: &ArrayView2<'a, f64>,
+    neighborhood: &Neighborhood,
+    id: usize,
+    seeds: &mut Vec<usize>,
+    visited: &[bool],
+    reacheability: &mut [f64],
+) {
+    for &o in &neighborhood.neighbors {
+        if visited[o] {
+            continue;
+        }
+        let reachdist = reacheability_distance(o, id, input, neighborhood);
+        if !reacheability[o].is_normal() {
+            reacheability[o] = reachdist;
+            seeds.push(o);
+        } else if reacheability[o].lt(&reachdist) {
+            reacheability[o] = reachdist;
+        }
+    }
+    seeds.sort_unstable_by(|a, b| {
+        reacheability[*a]
+            .partial_cmp(&reacheability[*b])
+            .unwrap()
+            .reverse()
+    });
 }
 
 struct Neighborhood {
@@ -52,4 +147,22 @@ fn build_neighborhoods<'a>(input: &ArrayView2<'a, f64>, eps: f64) -> Vec<Neighbo
             core_distance: db.query_one(&p).distance,
         })
         .collect()
+}
+
+fn distance(a: &ArrayView1<f64>, b: &ArrayView1<f64>) -> f64 {
+    (a - b).mapv(|x| x.powi(2)).sum().sqrt()
+}
+
+fn reacheability_distance<'a>(
+    o: usize,
+    p: usize,
+    input: &ArrayView2<'a, f64>,
+    neighbors: &Neighborhood,
+) -> f64 {
+    let dist = distance(&input.row(o), &input.row(p));
+    if dist.gt(&neighbors.core_distance) {
+        dist
+    } else {
+        neighbors.core_distance
+    }
 }
