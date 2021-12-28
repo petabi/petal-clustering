7d8d08957d271374b60a892483511ada7960b11a
Min Shao <mshao@petabi.com>
2021-12-28 02:06:44 -0800

    make a blob

--- benches/setup.rs
+++ benches/setup.rs
@@ -1,9 +1,29 @@
-use ndarray_rand::rand::distributions::{Distribution, Uniform};
+use ndarray::ArrayView1;
 use ndarray_rand::rand::{rngs::StdRng, RngCore, SeedableRng};
+use ndarray_rand::rand_distr::{Distribution, Normal, Uniform};
+
+/// make_a_blob: generate an isotropic Gaussian blob,
+///  centered at `center` with standard deviation `std_dev`
+///  blob size: `n_smaples`
+/// data is returned in form of Vec<f64> (COLUMN major: `n_features` * `n_samples`)
+fn make_a_blob<R: RngCore + Clone>(
+    center: ArrayView1<f64>,
+    n_samples: usize,
+    std_dev: f64,
+    seed_rng: R,
+) -> Vec<f64> {
+    let mut data = Vec::new();
+    for c in center {
+        let mut rng = StdRng::from_rng(seed_rng.clone()).unwrap();
+        let norm = Normal::new(*c, std_dev).unwrap();
+        data.extend(norm.sample_iter(&mut rng).take(n_samples));
+    }
+    data
+}
 
 /// make_centers Generate uniformly distributed `n_centers * n_features` centers
 /// within the bounding box provided by `center_box`. Rng used is seeded by `seed_rng`.
-/// results are returned in form of Vec<f64>.
+/// results are returned in form of Vec<f64> (row major n_centers * n_features).
 fn make_centers<R: RngCore>(
     n_centers: usize,
     n_features: usize,
