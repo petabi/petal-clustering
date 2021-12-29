77c7d6441d3b7803ba71196adb7037ae068454e2
Min Shao <mshao@petabi.com>
2021-12-29 00:44:53 -0800

    make blobs

--- benches/setup.rs
+++ benches/setup.rs
@@ -1,6 +1,42 @@
-use ndarray::ArrayView1;
-use ndarray_rand::rand::{rngs::StdRng, RngCore, SeedableRng};
+use ndarray::{concatenate, Array2, ArrayView, ArrayView1, Axis};
+use ndarray_rand::rand::{
+    rngs::{OsRng, StdRng},
+    RngCore, SeedableRng,
+};
 use ndarray_rand::rand_distr::{Distribution, Normal, Uniform};
+
+const DEFAULT_CLUSTER_STD: f64 = 1.0;
+const DEFAULT_CENTERS: usize = 3;
+const DEFAULT_CENTER_BOX: (f64, f64) = (-10., 10.);
+
+pub(crate) fn make_blobs(
+    n_samples: usize,
+    n_features: usize,
+    centers: Option<usize>,
+    cluster_std: Option<f64>,
+    center_box: Option<(f64, f64)>,
+) -> Array2<f64> {
+    let centers = centers.unwrap_or(DEFAULT_CENTERS);
+    let cluster_std = cluster_std.unwrap_or(DEFAULT_CLUSTER_STD);
+    let center_box = center_box.unwrap_or(DEFAULT_CENTER_BOX);
+
+    let centers_data = make_centers(centers, n_features, center_box, OsRng);
+    let samples_per_center = n_samples / centers;
+    let centers = ArrayView::from_shape((centers, n_features), &centers_data).unwrap();
+    let mut data = vec![];
+    for center in centers.rows() {
+        data.push(make_a_blob(center, samples_per_center, cluster_std, OsRng));
+    }
+    let blobs: Vec<_> = data
+        .iter()
+        .map(|blob| {
+            ArrayView::from_shape((n_features, samples_per_center), blob.as_slice())
+                .unwrap()
+                .reversed_axes()
+        })
+        .collect();
+    concatenate(Axis(0), blobs.as_slice()).unwrap()
+}
 
 /// make_a_blob: generate an isotropic Gaussian blob,
 ///  centered at `center` with standard deviation `std_dev`
