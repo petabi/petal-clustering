415c6159d4de8922426b394c9aa8348c7136173f
Min Shao <mshao@petabi.com>
2022-01-02 00:11:21 -0800

    add benchmark test for optics

--- benches/optics.rs
+++ benches/optics.rs
@@ -1,7 +1,7 @@
 use criterion::{black_box, Criterion};
-use ndarray::ArrayView;
+use ndarray::{arr2, ArrayView};
 use ndarray_rand::rand::{rngs::StdRng, Rng, SeedableRng};
-use petal_clustering::{Fit, Optics};
+use petal_clustering::{make_blobs, CenterConfig, Fit, Optics};
 use petal_neighbors::distance::Euclidean;
 
 #[allow(dead_code)]
@@ -16,6 +16,36 @@ pub(crate) fn build(c: &mut Criterion) {
         b.iter(|| {
             let mut model = Optics::new(0.5, 10, Euclidean::default());
             model.fit(&array);
+        })
+    });
+}
+
+#[allow(dead_code)]
+pub(crate) fn uniform_clusters(c: &mut Criterion) {
+    let n = black_box(5000);
+    let dim = black_box(3);
+
+    let array = make_blobs(n, dim, None, None);
+    c.bench_function("uniform_clusters", |b| {
+        b.iter(|| {
+            let mut model = Optics::new(1., 10, Euclidean::default());
+            model.fit(&array.view());
+        })
+    });
+}
+
+#[allow(dead_code)]
+pub(crate) fn fixed_clusters(c: &mut Criterion) {
+    let n = black_box(50_000);
+    let dim = black_box(3);
+    let centers = arr2(&[[1., 1., 1.], [-1., -1., -1.], [1., -1., 1.]]);
+
+    let array = make_blobs(n, dim, Some(CenterConfig::Fixed(centers)), Some(0.4));
+
+    c.bench_function("fixed_clusters", |b| {
+        b.iter(|| {
+            let mut model = Optics::new(0.3, 10, Euclidean::default());
+            model.fit(&array.view());
         })
     });
 }
